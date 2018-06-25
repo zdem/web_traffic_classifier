78578c42c7ca68992ce3761d80f352e0689fbc02
zdem <zde84nk@gmail.com>
2018-06-25 14:26:52 +0200

    Simplified the http_requests module by using the urlparse module. requirements.txt have been updated to reflect this change.
    Added a new function normal_or_anomalous which will serve as the predictor for the final model.

--- step1_extract_features/http_requests.py
+++ step1_extract_features/http_requests.py
@@ -1,62 +1,71 @@
 import re
 import numpy as np
+from urlparse import urlparse, parse_qs
 
-# Extract five features from a single HTTP_request as described in Althubiti et al paper.
+# Extract the five features from a single HTTP_request as described in Althubiti et al paper.
+# These are formed from the URI only: the rest of the HTTP request is not useful.
 def extract_features(HTTP_request, debug = False):
 	features = np.zeros(5)
 
-	# Extract the full path
-	path = re.match('(http.*) ',HTTP_request).group(1)
+	# Extract the method and the URI
+	first_line = re.match('(.*) (http.*) HTTP',HTTP_request)
+	method = first_line.group(1)
+	uri_string = first_line.group(2)
 
-	# Extract the arguments
-	r = re.search('Content-Length:\s*(\d+)',HTTP_request)
-	if (r):
-		# This is a POST or a PUT request: get the arguments from the line following 'Content-Length'
-                typ = 'POST or PUT'
-                arguments = re.sub('\n|\r','',HTTP_request[r.end(1)+1:])
-		argument_len = len(arguments)
+	# Remove the redundant information from the request body
+	stripped_request = re.sub('User-Agent:.*Connection: close|[\n\r]','',HTTP_request)
 
-                if argument_len != int(r.group(1)):
-                    print HTTP_request
-                    print arguments
-                    print "error in parsing POST or PUT arguments"
-                    exit()
+	if (method != 'GET'):
+		post_query = re.search('Content-Length:\s*\d+(.*)',stripped_request)
+                arguments = re.sub('\n|\r','',post_query.group(1))
+		uri_string = uri_string + '?' + arguments
 
-		# Generate the path as in GET request
-		path = path + '?' + arguments
-	else:
-		# This is a GET request: get the arguments directly from the path
-                typ = 'GET '
-		arguments = re.search('\?(.*)',path)
-		if (arguments):
-			arguments = arguments.group(1)
-		else:
-			arguments = ''
+	stripped_request = uri_string
 
-	arguments = re.sub('\n|\r','', arguments)
+	# Parse the URI into path and arguments
+	uri = urlparse(uri_string)
+
+	path = uri.path
+	arguments = parse_qs(uri.query)
 
 	#1. Length of the request
-	features[0] = len(HTTP_request)
+	features[0] = len(stripped_request)
 
 	#2. Length of the arguments
-	features[1] = len(arguments) - len(re.findall('&', arguments))
+	features[1] = 0
+	for parameter, value in arguments.iteritems():
+		features[1] += len(parameter+(''.join(value)))
 
 	#3. Number of arguments
-	features[2] = len(re.split('&', arguments))
+	features[2] = len(arguments)
 
 	#4. Length of the path
 	features[3] = len(path)
 
 	#5. Number of special chars in the path
-	features[4] = len(re.findall('\W', path))
+	features[4] = len(re.findall('\W',path))
 
 	if (debug):
-                print "\n" + typ
-		print "Path:", path
-		print "Arguments:", arguments
+                print "\n" + method
+		print path
+		print arguments
 		print features
 
 	return features
+
+# Predicts whether a given HTTP request string is normal (=0) or anomalous (=1)
+def normal_or_anomalous(HTTP_request):
+
+	features = extract_features(HTTP_request)
+	theta = [0.16163343, -0.16745845, -0.23716181, -0.18078638, -0.02442735]
+
+	p = 1.0/(1+np.exp(-(np.dot(theta,features)-4.19002258)))
+	if p >= 0.5:
+		p = 1
+	else:
+		p = 0
+
+	return p
 
 class dataset:
 
@@ -69,14 +78,20 @@ class dataset:
 
 		with open(path_to_file, 'r') as input_file: data = input_file.read()
 
-		# Split the raw data into individual HTTP requests using 'GET ', 'POST ' and 'PUT ' as delimiters
-		self.HTTP_requests = re.split('GET |POST |PUT ', data)
-		self.HTTP_requests.pop(0)
+		# Split the raw data into individual methods (GET, POST, PUT) request strings
+		methods_requests = re.split('(GET |POST |PUT )', data)
+		methods_requests.pop(0)
+
+		only_methods = methods_requests[::2]
+		only_requests = methods_requests[1:][::2]
+
+		self.HTTP_requests =  [method + request for method,request in zip(only_methods,only_requests)]
 
 		self.n_requests = len(self.HTTP_requests)
-		print "Found ", self.n_requests, " HTTP requests in file: ", path_to_file
+		print "\nFound ", self.n_requests, " HTTP requests in file: ", path_to_file
 
-	# Generate two sets of data (training, testing) splitting the whole data set according to the fraction of training data required.
+	# Generate two sets of data (training, testing) splitting the whole data set according to the fraction of training data required and
+	# assign a label to the data too.
 	def extract_labelled_HTTP_features(self,fraction_training,label):
 
 		X_training = []
@@ -104,7 +119,7 @@ class dataset:
 				X_testing.append(HTTP_features)
 				y_testing.append(label)
 
-		print "Data from",self.path_to_file,"have been split into a fraction of",fraction_training, "for training and a fraction of ",fraction_test,"for testing"
+		print "\nData from",self.path_to_file,"have been split into a fraction of",fraction_training, "for training and a fraction of ",fraction_test,"for testing"
                 print "Label assigned:", label
 		print "Total number of training requests obtained:",last_training_request
 
